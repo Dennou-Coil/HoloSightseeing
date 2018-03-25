fa2b9a67d809921926a6529cccac3602a6251c99
Takumi Ito <takumi10194617@gmail.com>
2018-03-26 01:05:37 +0900

    [feat] GPSObject

---
+++ Assets/HoloGPSReceiver/Scripts/GPSMapService/GPSObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GATARI.HoloLensGPS
+{
+    public class GPSObject : MonoBehaviour
+    {
+        public GPSObjectData gpsObjectData;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Activate()
+        {
+
+        }
+
+        public void Deactivate()
+        {
+
+        }
+    }
+}
