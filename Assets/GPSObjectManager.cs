821f5b2743aa89362957d335f6aa341727c94e60
Takumi Ito <takumi10194617@gmail.com>
2018-03-26 18:00:19 +0900

    [feat] refs #3 オブジェクトのシーン配置　データモデルの修正

---
+++ Assets/GPSObjectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Toolkit;
+
+namespace GATARI.HoloLensGPS {
+
+    public class GPSObjectManager : MonoBehaviour {
+        public static GPSObjectManager Instance { get; private set; }
+
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(this);
+            }
+        }
+        // Use this for initialization
+        void Start() {
+            Instance = this;
+        }
+
+        public void PlaceObject(GPSObjectData data) {
+
+        }
+    }
+}
+
---
+++ Assets/HoloGPSReceiver/Script/GPSObject/GPSObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+using System;
+using UniRx;
+
+namespace GATARI.HoloLensGPS {
+    public class GPSObject : MonoBehaviour {
+        public GPSObjectData gpsObjectData;
+        public TextMesh textMesh;
+        string anchorId;
+        Animator anim;
+
+        private void Start() {
+            anim = GetComponent<Animator>();
+            GPSMapService.Instance.onPostureUpdate.AddListener(CheckArea);
+            gpsObjectData.IsInside.Subscribe(b => {
+                if (b) {
+                    Activate(gpsObjectData);
+                } else {
+                    Deactivate();
+                }
+            });
+        }
+
+        void CheckArea(PlayerPosture posture) {
+            gpsObjectData.CheckArea(posture.playerPosition, posture.playerAngle);
+        }
+
+        public void Activate(GPSObjectData data) {
+            transform.position = new Vector3((float)(data.Distance * Math.Sin(data.Angle)), 0, (float)(data.Distance * Math.Cos(data.Angle)));
+            textMesh.text = data.GPSObjectName;
+            anim.SetBool("isVisible", true);
+        }
+
+        public void Deactivate() {
+            anim.SetBool("isVisible", false);
+        }
+    }
+}
